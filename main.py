edd2782c3f09bb7205e4913785ac80c7fd663582
junjunmeng <tug25071@temple.edu>
2023-01-05 22:29:38 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+def PatternCount(text, pattern):
+    count = 0
+    for i in range(len(text) - len(pattern)+ 1):
+        if text[i:i+len(pattern)] == pattern:
+            count += 1
+    return count
+
